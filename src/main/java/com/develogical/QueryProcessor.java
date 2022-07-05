c9d50837c414c701a8b69e223d1ec57b784371a0
Varnika Tyagi <varnikatyagi.94@gmail.com>
2022-07-05 11:01:00 +0200

    Created new query and query test

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -8,6 +8,8 @@ public class QueryProcessor {
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         }
+        else if (query.toLowerCase().contains("hello")) {
+            return "hello to you too!";
         return "";
     }
 }
